da4e4d4db9b02e4deaa2fa9d5390f5ec2a8739f2
MSD <123319753+msamet07@users.noreply.github.com>
2023-10-18 20:47:55 +0300

    tamamlandı
    Anlık chat uygulaması

---
+++ server/index.js
@@ -0,0 +1,38 @@
+const express= require('express');
+const cors = require('cors');
+const http= require('http');
+const {Server} = require('socket.io');
+const { Socket } = require('engine.io');
+
+
+
+const app=express();
+app.use(cors());
+
+
+const server = http.createServer(app)
+
+
+const io=new Server(server,{
+    cors:{
+        origin:'http://localhost:3000',
+        methods:['GET','PORT']
+    }
+})
+
+io.on('connection' , (socket) =>{
+    console.log(socket.id)
+
+    socket.on('room',(data) =>{
+        socket.join(data)
+    })
+    socket.on('message',(data)=>{
+        socket.to(data.room).emit('messageReturn', data)
+    })
+})   
+
+
+const PORT =5000;
+server.listen(PORT,() =>{
+    console.log('Server is running now:5000')
+})
